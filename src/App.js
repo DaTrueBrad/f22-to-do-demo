803501928b41260c863061242334e077d918186c
Brady Bott <bottbrady@gmail.com>
2022-06-20 11:09:30 -0600

    finished week 1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 // import Header from './components/Header';
 import Header from './components/Header';
 import AddTask from './components/task/AddTask';
+import {Routes, Route} from 'react-router-dom'
+import HomeScreen from './components/home/HomeScreen';
+import TeamScreen from './components/team/TeamScreen';
+import Details from './components/home/Details';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AddTask />
+      <Routes>
+        <Route index element={<HomeScreen />} />
+        <Route path="tasks" element={<AddTask />} />
+        <Route path="team/*" element={<TeamScreen />} />
+        <Route path="details/:pokemon" element={<Details />} />
+      </Routes>
     </div>
   );
 }
